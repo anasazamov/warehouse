839da5688c01c16543a079fdb376d1c8d6a454a1
anasazamov <anasazamov55@gmail.com>
2024-10-26 15:19:53 +0500

    fix get warehouse

--- apps/company/location/get_warehouse_name_from_yandex.py
+++ apps/company/location/get_warehouse_name_from_yandex.py
@@ -1,15 +1,17 @@
 from geopy.geocoders import Nominatim
+import time
 
 def get_location_info(latitude, longitude):
-    # Geolokatsiya obyektini yaratish
-    geolocator = Nominatim(user_agent="geoapiExercises")
-
-    # Koordinatalarni geokodlash
-    location = geolocator.reverse((latitude, longitude), exactly_one=True)
+    time.sleep(15)
+    try:
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        location = geolocator.reverse((latitude, longitude), exactly_one=True)
+    except:
+        return ""
 
     if location:
         address = location.raw.get('address', {})
-        # Viloyat yoki davlat nomini olish
+        
         region = address.get('state', '')
         country = address.get('country', 'Davlat ma\'lumotlari topilmadi')
         return region
